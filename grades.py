4baa59c3549b8d4180c12296ba5f3126e18b8c11
Evan Martinez <91926556+evanmtz@users.noreply.github.com>
2023-02-12 19:35:23 -0600

    Update grades.py
    Managed to get the script to find the username and password input fields by changing how it finds the elements. Instead of using "presense_of_element" which didn't work, I used "find_element". I also used the find_all() and find() methods to find attributes with certain ids/class names and extracted the data in those attributes. The problem now is that it's not extracting the data even with everything required given.

--- grades.py
+++ grades.py
@@ -1,5 +1,5 @@
 import requests
-import bs4 as BeautifulSoup
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
@@ -7,7 +7,10 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-url = "https://hac.aldineisd.org/HomeAccess/Account/LogOn?ReturnUrl=%2fHomeAccess"
+import pandas as pd
+from info import *
+
+url = "https://hac.aldineisd.org/HomeAccess/Home/WeekView"
 
 ser = Service("C:\Program Files (x86)\chromedriver.exe")
 
@@ -15,22 +18,49 @@ driver = webdriver.Chrome(service=ser)
 
 driver.get(url)
 
-time.sleep(10)
+time.sleep(20)
 
 try:
-    un = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "LogOnDetails_UserName")))
-    un.send_keys("USERNAME")
+    un = driver.find_element(By.ID, "LogOnDetails_UserName")
+    un.send_keys(user)
     time.sleep(5)
 except:
     print("Couldn't locate Username element.")
     driver.quit()
 
 try:
-    pwd = WebDriverWait(driver, 10).until(EC.presence_of_element_located(By.XPATH, "//input[@name='LogOnDetails.Password']"))
-    pwd.send_keys("PASSWORD")
+    pwd = driver.find_element(By.ID, "LogOnDetails_Password")
+    pwd.send_keys(passwd)
     time.sleep(5)
 
 except:
     print("Couldn't locate Password element.")
     driver.quit()
+
+sign_in = driver.find_element(By.NAME, "login")
+sign_in.click()
+
+time.sleep(10)
+
+gradeList = []
+
+headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36"}
+r = requests.get(url, headers)
+soup = BeautifulSoup(r.content, 'html.parser')
+
+anchors = soup.find_all('a', id_ = 'courseName')
+
+for course in anchors:
+    className = course.find('a').text.strip()
+    grade = course.find('a', id_ = 'average')
+
+    classGrades = {
+        "className": className,
+        "grade": grade
+    }
+
+    gradeList.append(classGrades)
+
+data = pd.DataFrame(gradeList)
+
+print(data.head())
